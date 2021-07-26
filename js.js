f2ac1dafe647d2cfd7bf6317f7d5060e7e048793
Sander <sander.c.vonk@gmail.com>
2021-07-26 15:12:02 -0700

    add somewhat-working pages to site

--- js.js
+++ js.js
@@ -1,9 +1,16 @@
+function pad2(number) {
+
+    return (number < 10 ? '0' : '') + number
+
+}
+
 var mainContainer = document.getElementById('js-main-container'),
     loginContainer = document.getElementById('js-login-container'),
     loginButton = document.getElementById('js-btn-login'),
     background = document.getElementById('js-background');
 
 var spotifyPlayer = new SpotifyPlayer();
+var spotifyApi = new SpotifyWebApi();
 
 var template = function (data) {
     return `
@@ -18,28 +25,42 @@ var template = function (data) {
       </div>
       <div class="now-playing__controls">
         <div class="controls-center">
-            <div id="skip-back" class="skip-btn"><b>|</b>&#9664;</div>
+            <div id="skip-back" class="skip-btn">&#9664;</div>
             <div class="now-playing__status ${data.is_playing ? 'playing' : 'paused'}">${data.is_playing ? '&#9612;&#9612;' : '&#9654;'}</div>
-            <div id="skip-forward" class="skip-btn">&#9654;<b>|</b></div>
+            <div id="skip-forward" class="skip-btn">&#9654;</div>
         </div>
-        <div class="progress">
-            <div class="progress__bar" style="width:${data.progress_ms * 100 / data.item.duration_ms}%"></div>
+        <div class="progress-container">
+            <div class="progress-time">${Math.floor((data.progress_ms / 1000 / 60) << 0) + ':' + pad2(Math.floor((data.progress_ms / 1000) % 60))}</div>
+                <div class="progress">
+                    <div class="progress__bar" style="width:${data.progress_ms * 100 / data.item.duration_ms}%"></div>
+                 </div>
+            <div class="progress-duration">${Math.floor((data.item.duration_ms / 1000 / 60) << 0) + ':' + pad2(Math.floor((data.item.duration_ms / 1000) % 60))}</div>
         </div>
-      </div>
+        </div>
+    </div>
     </div>
     <div class="background-parent"><div class="background" style="background-image:url(${data.item.album.images[0].url})"></div></div>
   `;
 };
 lastImg = ""
 lastProgress = 0
+lastStatus = false
 spotifyPlayer.on('update', response => {
     if (response.item.album.images[0].url != lastImg) {
         mainContainer.innerHTML = template(response);
         lastImg = response.item.album.images[0].url
     } else if (response.progress_ms != lastProgress) {
+        //update progress
         document.getElementsByClassName("progress__bar")[0].style.width = `${response.progress_ms * 100 / response.item.duration_ms}%`
+        document.getElementsByClassName("progress-time")[0].textContent = Math.floor((response.progress_ms / 1000 / 60) << 0) + ':' + pad2(Math.floor((response.progress_ms / 1000) % 60))
     }
-
+    if (!(response.is_playing === lastStatus)) {
+        mainContainer.innerHTML = template(response);
+        document.getElementsByClassName("now-playing__status")[0].className = `now-playing__status ${response.is_playing ? 'playing' : 'paused'}`
+        document.getElementsByClassName("now-playing__status")[0].innerHTML = `${response.is_playing ? '&#9612;&#9612;' : '&#9654;'}`
+        lastStatus = response.is_playing
+        console.log("different play state")
+    }
 
 });
 
