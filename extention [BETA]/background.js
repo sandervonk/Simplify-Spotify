94a497f0d84b899e68ea903f76bade8b5f45586d
Sander <sander.c.vonk@gmail.com>
2021-07-27 12:54:49 -0700

    Add extention features
    proof of concept, works fine, aside from the controls, which do not, as the web API is not correctly loading

--- extention [BETA]/background.js
+++ extention [BETA]/background.js
@@ -1,16 +1,26 @@
 const client_id = 'fc6786adda404b36a1f67a106913dd6f'
-const redirectUri = chrome.identity.getRedirectURL();
+var redirectUri = chrome.identity.getRedirectURL();
+redirectUri = redirectUri.substring(0, redirectUri.length - 1)
 
 console.log(redirectUri);
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(sender)
+    console.log([`message:`, message])
     if (message.action === 'run_auth_flow') {
+        console.log("running auth flow for url:")
+        console.log(`https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirectUri}&response_type=token`)
         chrome.identity.launchWebAuthFlow({
             "url": `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirectUri}&response_type=token`,
             'interactive': true,
         }, (redirect_url) => {
             console.log(redirect_url);
+            token = redirect_url.split("#access_token=")[1].split("&")[0]
+            createDate = new Date()
+            chrome.storage.sync.set({ "token": token, "token_date": String(createDate) }, function () {
+                console.log("value saved")
+                console.log({ "token": token, "token_date": String(createDate) })
+            });
         });
     }
     //sendResponse({ status: 'ok' })
@@ -18,5 +28,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 chrome.identity.onSignInChanged.addListener((account, signedIn) => {
+    console.log("SignInChanged")
     console.log(account, signedIn)
 });
